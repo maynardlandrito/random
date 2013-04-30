cf349d065e0a31ae7f09f93a91c67a8e925c5e74
Maynard R. Landrito <landritom@yahoo.com>
2013-05-01 00:01:09 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Connect4::Application.config.secret_token = '5a34ff17e05ad919548b71d125f7a48ff5e865ac8f77b141a8ca27739a7e744bde4f8368fc38951b79a89c50cc0151c6c3407bb1bc033a973761d8cdb986bce9'
